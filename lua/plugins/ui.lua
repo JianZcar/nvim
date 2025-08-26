ee08b4fb622d204fb00486c776dfce4107b983d7
JianZcar <esteban.jianzcar@outlook.com>
2025-08-26 21:28:42 +0800

    autocomplete fixes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -52,7 +52,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
